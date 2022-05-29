510b06ea682d5feac4d72b44cef6680fc3017f1e
Constantine <barr.develop@gmail.com>
2022-05-29 18:14:42 +0700

    add token track

--- index.ts
+++ index.ts
@@ -10,9 +10,15 @@ require("dotenv").config();
 // session ?? https://github.com/telegraf/telegraf/blob/v4/docs/examples/session-bot.ts
 // https://www.digitalocean.com/community/tutorials/how-to-build-a-telegram-quotes-generator-bot-with-node-js-telegraf-jimp-and-pexels good tutorial
 
+const track: Record<string, string[]> = {};
+
 interface SessionData {
-  from: string | null;
-  to: string | null;
+  state: "trackRequested" | "rateRequested" | null;
+  data: {
+    from: string | null;
+    to: string | null;
+    track: string[];
+  };
 }
 
 // Define your own context type
@@ -31,16 +37,16 @@ bot.start((ctx) => {
       description: "Get exchange rate",
     },
     {
-      command: "help",
-      description: "Get help from finance bot!",
+      command: "track",
+      description: "Get currency updates every day/hour/minute",
     },
     {
       command: "help",
-      description: "Get help from finance bot!",
+      description: "Get help from finance bot",
     },
   ]);
   ctx.reply(
-    "Welcome! It is a crypto-tracker bot. Please, select fiat currency in which you want to get current coin price."
+    'Welcome! It is a "Crypto Exchange" bot. Please, select fiat currency in which you want to get current coin price.'
   );
 });
 
@@ -49,7 +55,23 @@ bot.help((ctx) => {
   ctx.session = null;
   ctx.reply("Finance bot is intent to help you with ");
 });
-bot.command("track", (ctx) => ctx.reply("Hello"));
+bot.command("track", (ctx) => {
+  ctx.reply("Please, send coin token which you want to track");
+
+  // install session if there is none
+  if (!ctx.session) {
+    ctx.session = {
+      state: "trackRequested",
+      data: {
+        from: null,
+        to: null,
+        track: [],
+      },
+    };
+  } else if (ctx.session.state === null) {
+    ctx.session.state = "trackRequested";
+  }
+});
 
 bot.command("rate", (ctx) => {
   ctx.reply(
@@ -59,52 +81,79 @@ bot.command("rate", (ctx) => {
       .resize()
   );
   ctx.session = {
-    from: null,
-    to: null,
+    state: "rateRequested",
+    data: {
+      from: null,
+      to: null,
+      track: [],
+    },
   }; // start sequence
 });
 bot.on("message", async (ctx) => {
   try {
-    if (!ctx.session || !("text" in ctx.message)) {
+    if (!ctx.session || !("text" in ctx.message) || !ctx.session.state) {
       // if there is no started sequence, then do nothing
       return;
     }
 
-    const text = ctx.message.text.toLowerCase(); // get message text
-    if (!ctx.session.from) {
-      ctx.reply(
-        "please, select fiat currency or type it yourself",
-        Markup.keyboard([["EUR", "USD"]])
-          .oneTime()
-          .resize()
-      );
-      ctx.session.from = text;
-    } else if (!ctx.session.to) {
-      // step 2 - get fiat currency
-      ctx.session.to = text;
-      ctx.reply(
-        "Checking cryptocurrency exchanges...",
-        Markup.removeKeyboard()
-      );
+    // get message text
+    const text = ctx.message.text.toLowerCase();
 
-      const response = await axios.get(
-        `https://rest.coinapi.io/v1/exchangerate/${ctx.session.from.toUpperCase()}/${ctx.session.to.toUpperCase()}`,
-        {
-          headers: {
-            "X-CoinAPI-Key": process.env.API_KEY,
-          },
+    switch (ctx.session.state) {
+      case "trackRequested": {
+        if (ctx.session.data.track.includes(text)) {
+          ctx.reply(`${text.toLocaleUpperCase()} is already in track list...`);
+          return;
+        } else {
+          ctx.session.data.track = [...ctx.session.data.track, text];
+          track[ctx.chat.id] ??= [];
+          track[ctx.chat.id].push(text);
+
+          ctx.reply(`${text.toLocaleUpperCase()} is added to track list...`);
+          ctx.session.state = null;
         }
-      );
-      if (response.status === 200) {
-        ctx.reply(
-          `1 ${ctx.session.from} ➡️ ${response.data.rate.toFixed(2)} ${
-            ctx.session.to
-          }`
-        );
-      } else {
-        throw new Error("wd");
+        return;
       }
-      ctx.session = null;
+      case "rateRequested": {
+        if (!ctx.session.data.from) {
+          ctx.reply(
+            "please, select fiat currency or type it yourself",
+            Markup.keyboard([["EUR", "USD"]])
+              .oneTime()
+              .resize()
+          );
+          ctx.session.data.from = text;
+        } else if (!ctx.session.data.to) {
+          // step 2 - get fiat currency
+          ctx.session.data.to = text;
+          ctx.reply(
+            "Checking cryptocurrency exchanges...",
+            Markup.removeKeyboard()
+          );
+
+          const response = await axios.get(
+            `https://rest.coinapi.io/v1/exchangerate/${ctx.session.data.from.toUpperCase()}/${ctx.session.data.to.toUpperCase()}`,
+            {
+              headers: {
+                "X-CoinAPI-Key": process.env.API_KEY,
+              },
+            }
+          );
+          if (response.status === 200) {
+            ctx.reply(
+              `1 ${ctx.session.data.from} ➡️ ${response.data.rate.toFixed(2)} ${
+                ctx.session.data.to
+              }`
+            );
+          } else {
+            throw new Error("wd");
+          }
+          ctx.session = null;
+        }
+        return;
+      }
+      default:
+        return;
     }
   } catch (error) {
     ctx.session = null;
@@ -120,6 +169,27 @@ process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
 // set schedule job
-const job = schedule.scheduleJob("1 * * * *", function () {
-  Telegraf.reply("tracked");
+const job = schedule.scheduleJob("0-59 * * * *", async function () {
+  for (const chatId in track) {
+    try {
+      const response = await axios.get(
+        `https://rest.coinapi.io/v1/exchangerate/${track[
+          chatId
+        ][0].toUpperCase()}/USD`,
+        {
+          headers: {
+            "X-CoinAPI-Key": process.env.API_KEY,
+          },
+        }
+      );
+      if (response.status === 200) {
+        bot.telegram.sendMessage(
+          chatId,
+          `${track[chatId][0].toUpperCase()} - ${response.data.rate.toFixed(2)}`
+        );
+      }
+    } catch (error) {
+      throw new Error(`Failed to track ${track[chatId][0]}`);
+    }
+  }
 });
